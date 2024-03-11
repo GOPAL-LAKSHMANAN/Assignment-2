6d03a2b651e7fe06c63a2da5e20ddf07d2146efd
GOPAL-LAKSHMANAN <139698710+GOPAL-LAKSHMANAN@users.noreply.github.com>
2024-03-11 22:05:31 +0530

    Update D.md.m

--- D.md.m
+++ D.md.m
@@ -1,3 +1,5 @@
 ## TO OPEN A DATASET
 
 ## [CLICK HEARE](https://www.kaggle.com/code/yasserhessein/thyroid-disease-detection-using-deep-learning)
+
+Kaggle Dataset URL: https://www.kaggle.com/datasets/bhavikjikadara/fake-news-detection
